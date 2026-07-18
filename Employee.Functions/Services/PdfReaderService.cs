1e43e47b43e012a9dbe8940c5075edf64a181f00
Kamal Singh Bora <thekamalbora@yahoo.com>
2026-07-18 13:56:12 +0530

    Initial commit - Azure Event-Driven Document Processing Pipeline

--- Employee.Functions/Services/PdfReaderService.cs
+++ Employee.Functions/Services/PdfReaderService.cs
@@ -7,14 +7,11 @@ public class PdfReaderService
     public string Read(Stream stream)
     {
         using var document = PdfDocument.Open(stream);
-
         var text = "";
-
         foreach (var page in document.GetPages())
         {
             text += page.Text;
         }
-
         return text;
     }
 }
